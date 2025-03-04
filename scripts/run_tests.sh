474da195c78ea356cb74cc6ce6ba1cc216758367
louistsang <louis.tsang@ricoh.com.hk>
2025-03-04 16:20:49 +0800

    update test script to exit on command failure and not build in release mode

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
+set -e # Exit if command failed
 cmake -E make_directory "build"
-cmake -E chdir "build" cmake -DRUN_TESTS=on -DCMAKE_BUILD_TYPE=Release ..
-cmake --build "build" --config release
-cmake -E chdir "build" ctest --build-config Release
+cmake -E chdir "build" cmake -DRUN_TESTS=on -DCMAKE_BUILD_TYPE=DEBUG ..
+cmake --build "build"
+cmake -E chdir "build" ctest
