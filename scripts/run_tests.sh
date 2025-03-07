1634fd176678da3bf8b5ed0750ac64af9fd68b82
louistsang <louis.tsang@ricoh.com.hk>
2025-03-07 16:28:43 +0800

    update run test script to build in nested dir

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e # Exit if command failed
-cmake -E make_directory "build"
-cmake -E chdir "build" cmake -DRUN_TESTS=on -DCMAKE_BUILD_TYPE=DEBUG ..
-cmake --build "build"
-cmake -E chdir "build" ctest
+cmake -E make_directory "build/test"
+cmake -E chdir "build/test" cmake -DRUN_TESTS=on -DCMAKE_BUILD_TYPE=DEBUG ../../
+cmake --build "build/test"
+cmake -E chdir "build/test" ctest
